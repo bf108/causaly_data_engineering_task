e045eb589e5c1342d83f26af460fe41cc186df34
Ben Farrell <ben.farrell@iqvia.com>
2024-10-12 15:55:32 +0100

    Add func to find single element in xml tree

--- src/batch_pipeline_utils.py
+++ src/batch_pipeline_utils.py
@@ -10,3 +10,12 @@ def get_root_element(tree: etree._ElementTree) -> etree._Element:
     root = tree.getroot()
     return root
 
+
+def get_single_element(tree: etree._ElementTree, element: str) -> etree._Element | None:
+    element = tree.find(element)
+    return element
+
+
+def get_element_text(element: str) -> str:
+    element_str = element.text
+    return element_str
--- tests/test_batch_pipeline_utils.py
+++ tests/test_batch_pipeline_utils.py
@@ -1,6 +1,6 @@
 import pytest
 from lxml import etree
-from causaly.src.batch_pipeline_utils import get_xlm_tree
+from causaly.src.batch_pipeline_utils import get_xlm_tree, get_single_element, get_element_text
 
 
 def test_get_xlm_tree_passes():
@@ -12,3 +12,37 @@ def test_get_xlm_tree_passes():
 def test_get_xlm_tree_fails_no_file():
     with pytest.raises(OSError):
         get_xlm_tree('data/does_not_exist.xml')
+
+
+def test_get_single_element_returns_correct_element_when_element_exists():
+    # Given
+    xml_string = "<root><child>content</child></root>"
+    tree = etree.ElementTree(etree.fromstring(xml_string))
+    expected_element = etree.Element("child")
+    expected_element.text = "content"
+
+    # When
+    actual_element = get_single_element(tree, "child")
+
+    # Then
+    assert actual_element.tag == expected_element.tag
+    assert actual_element.text == expected_element.text
+
+def test_get_single_element_returns_none_when_element_does_not_exist():
+    # Given
+    xml_string = "<root><child>content</child></root>"
+    tree = etree.ElementTree(etree.fromstring(xml_string))
+
+    # When
+    actual_element = get_single_element(tree, "nonexistent")
+
+    # Then
+    assert actual_element is None
+
+def test_get_single_element_raises_error_when_tree_is_none():
+    # Given
+    tree = None
+
+    # When/Then
+    with pytest.raises(AttributeError):
+        get_single_element(tree, "child")
