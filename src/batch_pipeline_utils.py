796c02a3f0ced3a15571aa494384def8b4be0294
Ben Farrell <ben.farrell@iqvia.com>
2024-10-13 15:14:27 +0100

    Run pre-commit hooks

--- src/batch_pipeline_utils.py
+++ src/batch_pipeline_utils.py
@@ -1,32 +1,36 @@
+from typing import Optional
+
 from lxml import etree
+from lxml.etree import _Element
+from lxml.etree import _ElementTree
 
 
-def get_xlm_tree(file_path: str) -> etree._ElementTree:
+def get_xlm_tree(file_path: str) -> _ElementTree:
     tree = etree.parse(file_path)
     return tree
 
 
-def get_root_element(tree: etree._ElementTree) -> etree._Element:
+def get_root_element(tree: _ElementTree) -> _Element:
     root = tree.getroot()
     return root
 
 
-def get_single_element(tree: etree._ElementTree, element: str) -> etree._Element | None:
-    element = tree.find(element)
+def get_single_element(tree: _ElementTree, element_name: str) -> Optional[_Element]:
+    element = tree.find(f".//{element_name}")
     return element
 
 
-def get_element_text(element: str) -> str:
+def get_element_text(element: _Element) -> str | None:
     element_str = element.text
     return element_str
 
 
 def get_all_elements(
-        tree: etree.ElementTree,
-        element: str,
-        *,
-        attribute_value: str | None = None,
-) -> list[etree._Element]:
+    tree: _ElementTree,
+    element: str,
+    *,
+    attribute_value: str | None = None,
+) -> list[_Element]:
     search_string = f".//{element}"
     if isinstance(attribute_value, str):
         search_string += f"[@{attribute_value}]"
