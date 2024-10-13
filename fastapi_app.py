4f4b6034836060a25751aa7e69d0316d873e5043
Ben Farrell <ben.farrell@iqvia.com>
2024-10-13 19:52:27 +0100

    Update Fastapi app

--- fastapi_app.py
+++ fastapi_app.py
@@ -2,32 +2,35 @@ import sqlite3
 from pathlib import Path
 
 from fastapi import FastAPI
+from src.batch_pipeline_utils import get_keyword_pairs_from_meeting_abstract
 from src.batch_pipeline_utils import standardize_string
+from src.sql_db_utils import create_connection
+from src.sql_db_utils import get_most_occurring_keywords_from_sql
+from src.sql_db_utils import update_data_store
 
+
+DB = str(Path(__file__).parent / "data/meeting_abstracts.db")
 
 app = FastAPI()
-
-
-def get_most_occurring_keywords_from_sql(search_string: str) -> list[str]:
-    parent_dir = Path(__file__).parent
-    conn = sqlite3.connect(parent_dir / "data/meeting_abstracts.db")
-    cursor = conn.cursor()
-    top_keyword_pairs = cursor.execute(
-        f"SELECT keyword_2 FROM keyword_pair_frequency_table WHERE keyword_1 = '{search_string}'"
-        f"AND frequency = (SELECT MAX(frequency) FROM keyword_pair_frequency_table WHERE keyword_1 = '{search_string}')"
-        f"ORDER BY keyword_2 ASC"
-    )
-    top_matches = [kw[0] for kw in top_keyword_pairs]
-    return top_matches
 
 
 @app.post("/get_most_occurring_keywords")
 async def get_most_occurring_keywords(keyword: str):
     keyword_standardized = standardize_string(keyword)
-    most_occurring_keywords = get_most_occurring_keywords_from_sql(keyword_standardized)
+    conn = create_connection(DB)
+    if not conn:
+        return {"message": "Failed to create sqlite3 connection"}
+    most_occurring_keywords = get_most_occurring_keywords_from_sql(
+        conn, keyword_standardized
+    )
+    conn.close()
     return {"most_occurring_keywords": most_occurring_keywords}
 
 
 @app.post("/add_meeting_abstract")
-async def add_meeting_abstract(meeting_abstract):
+async def add_meeting_abstract(meeting_abstract: str):
+    conn = sqlite3.connect(DB)
+    keyword_pairs = get_keyword_pairs_from_meeting_abstract(meeting_abstract)
+    update_data_store(conn, keyword_pairs)
+    conn.close()
     return {"message": "Meeting abstract added successfully"}
