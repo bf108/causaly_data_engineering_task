1c33785c23be9bd7b8eb67e88b36ecb30612990f
Ben Farrell <ben.farrell@iqvia.com>
2024-10-13 19:50:49 +0100

    Utility functions to allow abstract to be uploaded and update data store

--- src/sql_db_utils.py
+++ src/sql_db_utils.py
@@ -23,3 +23,61 @@ def get_most_occurring_keywords_from_sql(
     )
     top_matches = [kw[0] for kw in top_keyword_pairs]
     return top_matches
+
+
+def get_keyword_pair_freq_count(
+    conn: sqlite3.Connection, keyword_pair: KeywordPair
+) -> int:
+    cursor = conn.cursor()
+    freq = cursor.execute(
+        """
+        SELECT frequency
+        FROM keyword_pair_frequency_table
+        WHERE keyword_1 = ? AND keyword_2 = ?
+        """,
+        (keyword_pair.keyword_1, keyword_pair.keyword_2),
+    )
+    freq_val = freq.fetchone()
+    if freq_val:
+        return freq_val[0]
+    return 0
+
+
+def update_keyword_pair_frequency_table(
+    conn: sqlite3.Connection, keyword_pair: KeywordPair
+) -> None:
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        UPDATE keyword_pair_frequency_table
+        SET frequency = frequency + 1
+        WHERE keyword_1 = ? AND keyword_2 = ?
+        """,
+        (keyword_pair.keyword_1, keyword_pair.keyword_2),
+    )
+    conn.commit()
+
+
+def insert_keyword_pair_frequency_table(
+    conn: sqlite3.Connection, keyword_pair: KeywordPair
+) -> None:
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        INSERT INTO keyword_pair_frequency_table (keyword_1, keyword_2, frequency)
+        VALUES (?, ?, 1)
+        """,
+        (keyword_pair.keyword_1, keyword_pair.keyword_2),
+    )
+    conn.commit()
+
+
+def update_data_store(
+    conn: sqlite3.Connection, keyword_pairs: list[KeywordPair]
+) -> None:
+    for keyword_pair in keyword_pairs:
+        freq = get_keyword_pair_freq_count(conn, keyword_pair)
+        if freq == 0:
+            insert_keyword_pair_frequency_table(conn, keyword_pair)
+        else:
+            update_keyword_pair_frequency_table(conn, keyword_pair)
