ddad1bafccd51bb1cbc00ffb3d7fda1db41fecee
Ben Farrell <ben.farrell@iqvia.com>
2024-10-15 10:38:08 +0100

    Provide new path to scripts within airflow DAG

--- dags/data_pipeline_data.py
+++ dags/data_pipeline_data.py
@@ -11,17 +11,19 @@ os.environ["PYTHONPATH"] = "/opt/src"
 
 
 t1 = BashOperator(
-    task_id="create_sqlite_db", bash_command="python /opt/src/create_db.py", dag=dag
+    task_id="create_sqlite_db",
+    bash_command="python /opt/src/data_pipeline_app/stages/create_db.py",
+    dag=dag,
 )
 t2 = BashOperator(
     task_id="store_keyword_pair_occurrences",
-    bash_command="python /opt/src/keyword_co_occurrence_pairs.py",
+    bash_command="python /opt/src/data_pipeline_app/stages/keyword_co_occurrence_pairs.py",
     dag=dag,
 )
 
 t3 = BashOperator(
     task_id="aggregate_keyword_pairs",
-    bash_command="python /opt/src/calculate_aggregates.py",
+    bash_command="python /opt/src/data_pipeline_app/stages/calculate_aggregates.py",
     dag=dag,
 )
 
