bf0b1cda89d0a52305661467a67b67038659023d
Ben Farrell <ben.farrell@iqvia.com>
2024-10-15 10:33:35 +0100

    Add additional tests for uploading meeting abstract

--- tests/test_sql_commands.py
+++ tests/test_sql_commands.py
@@ -137,3 +137,90 @@ def test_create_connection_with_valid_db_file():
 def test_create_connection_with_invalid_db_file():
     conn = create_connection("/invalid/path/to/db_file.db")
     assert conn is None
+
+
+def test_is_meeting_in_table():
+    # Given
+    conn = sqlite3.connect(":memory:")
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE raw_extracts_table (
+            nlm_dcms_id TEXT,
+            keyword_1 TEXT,
+            keyword_2 TEXT
+        )
+        """
+    )
+    cursor.execute(
+        """
+        INSERT INTO raw_extracts_table (nlm_dcms_id, keyword_1, keyword_2)
+        VALUES
+        ("id1", "kw1", "kw2"),
+        ("id2","kw1", "kw3");
+        """
+    )
+    conn.commit()
+
+    # When
+    search_uid = "id1"
+    result = is_meeting_in_table(conn, search_uid)
+
+    # Then
+    assert result == True
+
+    # When
+    search_uid = "id3"
+
+    # Then
+    result = is_meeting_in_table(conn, search_uid)
+    assert result == False
+
+    conn.close()
+
+
+def test_update_raw_extracts_table():
+    # Given
+    conn = sqlite3.connect(":memory:")
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE raw_extracts_table (
+            nlm_dcms_id TEXT,
+            keyword_1 TEXT,
+            keyword_2 TEXT
+        )
+        """
+    )
+
+    cursor.execute(
+        """
+        INSERT INTO raw_extracts_table (nlm_dcms_id, keyword_1, keyword_2)
+        VALUES
+        ("id1", "kw1", "kw2"),
+        ("id2","kw2", "kw3");
+        """
+    )
+
+    conn.commit()
+
+    keyword_pairs = [
+        KeywordPair(nlm_dcms_id="id3", keyword_1="kw1", keyword_2="kw2"),
+        KeywordPair(nlm_dcms_id="id4", keyword_1="kw2", keyword_2="kw3"),
+    ]
+
+    # When
+    update_raw_extracts_table(conn, keyword_pairs)
+
+    # Then
+    cursor.execute("SELECT * FROM raw_extracts_table")
+    result = cursor.fetchall()
+    expected_result = [
+        ("id1", "kw1", "kw2"),
+        ("id2", "kw2", "kw3"),
+        ("id3", "kw1", "kw2"),
+        ("id4", "kw2", "kw3"),
+    ]
+    assert result == expected_result
+
+    conn.close()
